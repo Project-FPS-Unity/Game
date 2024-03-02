f549d7058490c162aeefb236022182f4d6244db7
Passakorn <maxpskk@gmail.com>
2024-03-02 15:51:50 +0700

    Item drop when enemy death

--- Assets/Scripts/Beta0.1/Enemy/Enemy.cs
+++ Assets/Scripts/Beta0.1/Enemy/Enemy.cs
@@ -18,13 +18,15 @@ public class Enemy : MonoBehaviour
     private float timeBonus = 10f;
 
     [Header("ItemDrop")]
-    [SerializeField] private GameObject ammoBox;
+    [SerializeField] private GameObject[] itemDropList;
+    private int totalItemsList = 0;
+    private int itemIndex;
 
     private void Awake()
     {
         health = new HealthSystem(maxHealth);
         healthBar = gameObject.GetComponentInChildren<HealthBar>();
-        ammoBox = GetComponent<GameObject>();
+
     }
 
     // Start is called before the first frame update
@@ -40,18 +42,32 @@ public class Enemy : MonoBehaviour
         //Health Check
         health.CheckHealth();
         healthBar.SetHealth(health.GetCurrentHealth());
-        if (health.isDead) Die();
+        if (health.isDead) 
+        {
+            Die();
+        }
     }
 
     private void Die()
     {
+        DropItem();
         TimeUpdater.timeRemain += timeBonus;
         TimeUpdater.enemyKillCount += 1;
-        Destroy(gameObject);
+        Destroy(gameObject); 
     }
 
     public void TakeDamage(float damage)
     {
         health.TakeDamage(damage);
+    }
+
+    private void DropItem()
+    {
+        foreach (GameObject item in itemDropList)
+        {
+            totalItemsList++;
+        }
+        itemIndex = Random.Range(0, totalItemsList);
+        Instantiate(itemDropList[itemIndex], new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.5f, gameObject.transform.position.z), Quaternion.identity);
     }
 }
